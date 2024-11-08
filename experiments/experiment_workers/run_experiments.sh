cf7c4d2716c5e3417f302e2f856ecaff644d2c15
evilmonkey19 <eperpinyapitarch@gmail.com>
2024-11-08 23:59:49 +0100

    finished results misconfigurations

--- experiments/experiment_workers/run_experiments.sh
+++ experiments/experiment_workers/run_experiments.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+for i in {9..9}; do
+    python generate_scenario.py $i misconfigurations
+    for j in {8..10}; do
+        cp events_log_template.csv events_log.csv
+        python master.py
+        mv events_log.csv event_log_${i}_${j}_only_misconfigurations.csv
+    done
+done
 
-for i in {4..10}; do
+for i in {10..10}; do
     python generate_scenario.py $i misconfigurations
     for j in {1..10}; do
         cp events_log_template.csv events_log.csv
@@ -9,11 +17,12 @@ for i in {4..10}; do
     done
 done
 
-# for i in {1..10}; do
-#     python generate_scenario.py $i errors
-#     for j in {1..10}; do
-#         cp events_log_template.csv events_log.csv
-#         python master.py
-#         mv events_log.csv event_log_${i}_${j}_only_errors.csv
-#     done
-# done
+for i in {1..10}; do
+    python generate_scenario.py $i errors
+    for j in {1..10}; do
+        cp events_log_template.csv events_log.csv
+        python master.py
+        mv events_log.csv event_log_${i}_${j}_only_errors.csv 
+    done
+done
+
